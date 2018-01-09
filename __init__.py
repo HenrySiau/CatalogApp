c5f8e196f2b29e69d227d401c44bd0622073df71
henry <siauhenry@gmail.com>
2018-01-09 01:05:21 -0800

    modified config from pyfile

--- __init__.py
+++ __init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from models import User
 
 app = Flask(__name__)
-app.config.from_object('config')
+app.config.from_pyfile('config.py')
 app.config.from_pyfile('instance_config.py')
 
 
