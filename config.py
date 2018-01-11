e7cbd273f16ca2a948774b2acdc1caac9bd8f13d
henry <siauhenry@gmail.com>
2018-01-10 23:54:57 -0800

    stage 4

--- config.py
+++ config.py
@@ -5,6 +5,7 @@ instance_config.py will also overwrite the configs in this page
 """
 DEBUG = True
 SQLALCHEMY_ECHO = True
-# SERVER_NAME = 'www.skynet.run'
+SERVER_NAME = 'www.skynet.run'
+# SERVER_NAME = 'localhost:5000'
 
 
